471fe90bbee69965cd95ce3955d5ca2f252364c4
phatdinhvn <phatdinhvn@gmail.com>
2018-03-06 22:00:41 +0700

    Phan giai

---
+++ Bai 2/Bai 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Bai_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Domain name/IP Address: ");
+            string hostName = Console.ReadLine();
+            IPHostEntry hostInfo = Dns.Resolve(hostName);
+            Console.WriteLine("Host name: "+hostInfo.HostName+"\nIP Address:");
+            
+            foreach (IPAddress ip in hostInfo.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+            Console.ReadLine();
+        }
+    }
+}
