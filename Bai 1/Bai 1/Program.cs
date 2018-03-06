332bb4e5549963537a7d0c69cbc115e424bf1c4c
phatdinhvn <phatdinhvn@gmail.com>
2018-03-06 22:08:51 +0700

    IP local

---
+++ Bai 1/Bai 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Bai_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IP Address list:");
+            string host = Dns.GetHostName();
+            IPHostEntry hostInfo = Dns.GetHostEntry(host);
+            foreach(IPAddress ip in hostInfo.AddressList)
+            Console.WriteLine(ip);
+            Console.ReadLine();
+        }
+    }
+}
